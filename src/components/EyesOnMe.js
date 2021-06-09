affb412abcec9ac9df871d5195c7fb3e17e05eba
Alex Calvert <alexanderajcalvert@gmail.com>
2021-06-09 13:17:20 -0700

    commit tree 4ebe5bdc0aca52c4eabd90cf2b1b1ad0205a8125

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function focus() {
+        console.log('Good!')
+    } 
+
+    function blur() {
+        console.log('Hey! Eyes on me!')
+    } 
+
+    return(
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    )
+
+    // let payingAttention = true;
+    
+    // if (payingAttention) {
+    //     return (
+    //         <button onClick={focus}>Eyes on me</button>
+    //     )
+    // } else {
+    //     return (
+    //         <button onClick={blur}>Eyes on me</button>
+    //     )
+    // }
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+
+function KeyPad(props) {
+    return (
+        <input onChange={() => console.log('Entering password...')} type="password" />
+    )
+}
+
+export default KeyPad;
